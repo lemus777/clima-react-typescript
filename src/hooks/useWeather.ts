171433350a2f175a52cc53782a78baf9e6422e51
lemus <lemus777@gmail.com>
2024-07-15 23:39:46 +0200

    types with type guard

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,18 @@
 import axios from "axios"
 import { SearchType, Weather } from "../types"
 
+// Type guard o assertion
+function isWeatherResponse(weather : unknown) : weather is Weather {
+  return (
+    Boolean(weather) &&
+    typeof weather === 'object' &&
+    typeof (weather as Weather).name === 'string' &&
+    typeof (weather as Weather).main.temp === 'number' &&
+    typeof (weather as Weather).main.temp_max === 'number' &&
+    typeof (weather as Weather).main.temp_min === 'number'
+  )
+}
+
 export default function useWeather() {
 
   const fetchWeather = async (search : SearchType) => {
@@ -8,7 +20,7 @@ export default function useWeather() {
     const appId = import.meta.env.VITE_API_KEY
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
-      
+
       const { data } = await axios(geoUrl)
       
       const lat = data[0].lat
@@ -16,9 +28,20 @@ export default function useWeather() {
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-      const {data: weatherResult} = await axios<Weather>(weatherUrl)
-      console.log(weatherResult.main.temp_max)
-      console.log(weatherResult.name)
+      // Castear el type
+      // const {data: weatherResult} = await axios<Weather>(weatherUrl)
+      // console.log(weatherResult.main.temp_max)
+      // console.log(weatherResult.name)
+
+      // Type guards
+      const {data: weatherResult} = await axios(weatherUrl)
+      const result = isWeatherResponse(weatherResult)
+      if (result) {
+        console.log(weatherResult.name)
+      } else {
+        console.log('Respuesta mal formada')
+      }
+      
     } catch (error) {
       console.log(error)
     }
