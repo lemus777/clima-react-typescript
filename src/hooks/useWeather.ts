9e2ac5ac2ed13efcffd65d20fe9930c14456f903
lemus <lemus777@gmail.com>
2024-07-16 23:25:20 +0200

    validating with Zod

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,17 +1,30 @@
 import axios from "axios"
-import { SearchType, Weather } from "../types"
+import { z } from "zod"
+import { SearchType } from "../types"
 
 // Type guard o assertion
-function isWeatherResponse(weather : unknown) : weather is Weather {
-  return (
-    Boolean(weather) &&
-    typeof weather === 'object' &&
-    typeof (weather as Weather).name === 'string' &&
-    typeof (weather as Weather).main.temp === 'number' &&
-    typeof (weather as Weather).main.temp_max === 'number' &&
-    typeof (weather as Weather).main.temp_min === 'number'
-  )
-}
+// function isWeatherResponse(weather : unknown) : weather is Weather {
+//   return (
+//     Boolean(weather) &&
+//     typeof weather === 'object' &&
+//     typeof (weather as Weather).name === 'string' &&
+//     typeof (weather as Weather).main.temp === 'number' &&
+//     typeof (weather as Weather).main.temp_max === 'number' &&
+//     typeof (weather as Weather).main.temp_min === 'number'
+//   )
+// }
+
+// Zod
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number()
+  })
+})
+
+type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
 
@@ -34,12 +47,20 @@ export default function useWeather() {
       // console.log(weatherResult.name)
 
       // Type guards
+      // const {data: weatherResult} = await axios(weatherUrl)
+      // const result = isWeatherResponse(weatherResult)
+      // if (result) {
+      //   console.log(weatherResult.name)
+      // } else {
+      //   console.log('Respuesta mal formada')
+      // }
+
+      // Zod
       const {data: weatherResult} = await axios(weatherUrl)
-      const result = isWeatherResponse(weatherResult)
-      if (result) {
-        console.log(weatherResult.name)
-      } else {
-        console.log('Respuesta mal formada')
+      const result = Weather.safeParse(weatherResult)
+      if (result.success) {
+        console.log(result.data.name)
+        console.log(result.data.main.temp)
       }
       
     } catch (error) {
