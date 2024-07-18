112c1f7937612f794f0d4edcdb6cba175bc6942a
lemus <lemus777@gmail.com>
2024-07-18 20:35:45 +0200

    loading spinneradded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import styles from "./App.module.css"
 import Form from "./components/Form/Form"
+import Spinner from "./components/Spinner/Spinner"
 import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
 import useWeather from "./hooks/useWeather"
 
 function App() {
 
-  const { weather, fetchWeather, hasWeatherData } = useWeather()
+  const { weather, loading, fetchWeather, hasWeatherData } = useWeather()
 
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
         <Form
           fetchWeather={fetchWeather}
         />
+        {loading && <Spinner />}
         {hasWeatherData && <WeatherDetail weather={weather} />}
       </div>
     </>
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -50,10 +50,12 @@ export default function useWeather() {
       temp_min: 0
     }
   })
+  const [loading, setLoading] = useState(false)
 
   const fetchWeather = async (search : SearchType) => {
 
     const appId = import.meta.env.VITE_API_KEY
+    setLoading(true)
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
@@ -96,6 +98,8 @@ export default function useWeather() {
       
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -103,6 +107,7 @@ export default function useWeather() {
 
   return {
     weather,
+    loading,
     fetchWeather,
     hasWeatherData
   }
